3884ce4009e390d5162135d28f06167df12e1a90
oooooba <fuumin.23@gmail.com>
2020-09-04 21:22:34 +0900

    add int_literals.wast spec test

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ fn main() {
     run_test(&format!("{}/i64.wast", base_dir_path));
     run_test(&format!("{}/f32.wast", base_dir_path));
     run_test(&format!("{}/f32_cmp.wast", base_dir_path));
+    run_test(&format!("{}/int_literals.wast", base_dir_path));
 }
 
 fn run_test(module_file_name: &str) {
