92a778652e8c28e92db7c1f7e5d0c5edb2152b67
oooooba <fuumin.23@gmail.com>
2020-09-27 19:56:22 +0900

    fix memory.grow instruction feature

--- src/instance.rs
+++ src/instance.rs
@@ -475,7 +475,7 @@ impl Meminst {
                 return Ok(None);
             }
         }
-        for _ in 0..(PAGE_SIZE * len) {
+        for _ in 0..(PAGE_SIZE * page_size) {
             self.data.push(0);
         }
         Ok(Some(sz as u32))
